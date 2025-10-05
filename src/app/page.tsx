1104bbb7c34874e1063c866096931dfac9353e60
akhfabgss <bagasakhfa02@gmail.com>
2025-10-05 13:23:58 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white">Next.js + Tailwind ✅</h1>
+    </div>
+  );
+}
